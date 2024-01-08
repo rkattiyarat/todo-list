966792f2f7a0a72a1ecb6d37d9145e304f933ff0
rose <aom_bnn@hotmail.com>
2024-01-07 19:10:49 -0500

    first draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-app';
+  todos : Todo[] = [];
+  newTodo : string;
+  inputEmpty: boolean = false;
+
+
+  addTodo() {
+    if (this.newTodo) {
+    let todo = new Todo();
+    todo.description = this.newTodo;
+    todo.isCompleted = true;
+    this.todos.push(todo);
+    this.newTodo = '';
+    this.inputEmpty = false; // Reset the inputEmpty flag
+    } else {
+      this.inputEmpty = true; // Set the inputEmpty flag
+    }
+  }
+
+  done(id: number) {
+    this.todos[id].isCompleted = !this.todos[id].isCompleted;
+  }
+  deleteTodo(id: number) {
+    this.todos = this.todos.filter((todo, index) => index !== id);
+  }
+  
 }
